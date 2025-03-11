6ff8dfe650140e0a4d7fb6a8c484512b2e508450
h <example@gmail.com>
2025-03-11 14:08:39 +0800

    dev

--- Sources/LocationPermissionSheet/LocationPermissionManager.swift
+++ Sources/LocationPermissionSheet/LocationPermissionManager.swift
@@ -1,6 +1,6 @@
-import SwiftUI
 import CoreLocation
 import PermissionSheets
+import SwiftUI
 
 public class LocationPermissionManager: NSObject, PermissionManaging, CLLocationManagerDelegate {
     @Published public var shouldShowSheet = false
@@ -8,7 +8,8 @@ public class LocationPermissionManager: NSObject, PermissionManaging, CLLocation
     public let permissionType: PermissionType = .location
     
     private let locationManager = CLLocationManager()
-    
+    private var completion: (CLAuthorizationStatus) -> Void = { _ in }
+
     public init(config: PermissionConfig) {
         self.config = config
         super.init()
@@ -20,17 +21,22 @@ public class LocationPermissionManager: NSObject, PermissionManaging, CLLocation
         let status = locationManager.authorizationStatus
         let shouldShow = config.forceShow ||
             (status != .authorizedWhenInUse && status != .authorizedAlways &&
-             config.displayFrequency.shouldDisplay(for: .location))
+                config.displayFrequency.shouldDisplay(for: .location))
         
         shouldShowSheet = shouldShow
     }
     
-    public func requestPermission() {
+    public func requestPermission(completion: @escaping (CLAuthorizationStatus) -> Void = { _ in }) {
+        self.completion = completion
         locationManager.requestWhenInUseAuthorization()
         updateLastPromptDate()
     }
     
     public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         checkPermission()
+    }
+    
+    func locationManagerDidChangeAuthorization(locationManager: CLLocationManager) {
+        completion(locationManager.authorizationStatus)
     }
 }
